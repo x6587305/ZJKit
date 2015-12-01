d782d85b0b9779cdf66fc8db79790445bd9d7fda
x6587305 <xiezhaojun_x@163.com>
2015-12-01 14:36:14 +0800

    新建项目 开始学习制作自己的kit

---
+++ ZJKitDemo/ZJKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZJKitDemo
+//
+//  Created by xiezhaojun on 15/12/1.
+//  Copyright © 2015年 xiezhaojun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZJKitDemo/ZJKitDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZJKitDemo
+//
+//  Created by xiezhaojun on 15/12/1.
+//  Copyright © 2015年 xiezhaojun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
