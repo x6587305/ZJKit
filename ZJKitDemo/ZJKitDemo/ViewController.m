f27c1511481000bfdd235158c0a3280d945690f7
x6587305 <xiezhaojun_x@163.com>
2015-12-03 13:26:56 +0800

    添加layer draw 代码

--- ZJKitDemo/ZJKitDemo/ViewController.m
+++ ZJKitDemo/ZJKitDemo/ViewController.m
@@ -29,8 +29,34 @@
  */
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
     
+    [self layerStudy];
+    
+}
+-(void)layerStudy{
+//    [self layerDraw];
+    
+}
+
+-(void)layerDraw{
+    //create sublayer
+    CALayer *blueLayer = [CALayer layer];
+    blueLayer.frame = CGRectMake(50.0f, 50.0f, 100.0f, 100.0f); blueLayer.backgroundColor = [UIColor blueColor].CGColor;
+    //set controller as layer delegate
+    blueLayer.delegate = self;
+    //ensure that layer backing image uses correct scale
+    blueLayer.contentsScale = [UIScreen mainScreen].scale; //add layer to our view
+    [self.view.layer addSublayer:blueLayer];
+    //force layer to redraw
+    [blueLayer display];
+
+}
+//
+- (void)drawLayer:(CALayer *)layer inContext:(CGContextRef)ctx {
+    //draw a thick red circle
+    CGContextSetLineWidth(ctx, 10.0f);
+    CGContextSetStrokeColorWithColor(ctx, [UIColor redColor].CGColor);
+    CGContextStrokeEllipseInRect(ctx, layer.bounds);
 }
 
 - (void)didReceiveMemoryWarning {
